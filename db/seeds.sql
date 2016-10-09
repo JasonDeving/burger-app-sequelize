f7da01220ef2abdd119b8d4b5ff341644c86d2e8
jasondeving <aznj50@hotmail.com>
2016-10-09 15:09:25 -0700

    hangry

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
 INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
 INSERT INTO burgers (burger_name) VALUES ('Pork Burger');
-INSERT INTO burgers (burger_name) VALUES ('Kamikaze Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
